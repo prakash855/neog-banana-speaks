c45c41d1a2d21cc3d5dbae9a3e642557b0121c57
Prakash Agrahari <sshivamprakashh301@gmail.com>
2021-04-09 17:27:02 +0530

    fetching of al the suoerheroes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-var buttonTranslate = document.querySelector('#btn')
-var text = document.querySelector('#txt')
-var output = document.querySelector('#output')
+var buttonTranslate = document.querySelector("#btn");
+var text = document.querySelector("#txt");
+var output = document.querySelector("#output");
 
-function clickEventHandler(){
-    output.innerText = `now this can be outputted here too => ${text.value}`
+function clickEventHandler() {
+  output.innerText = `now this can be outputted here too => ${text.value}`;
 }
 
-buttonTranslate.addEventListener("click", clickEventHandler) 
+buttonTranslate.addEventListener("click", clickEventHandler);
+
+// this is what I have done it my way to fetch the data for all three superheroes
+
+// const text1 = "Iron Man",
+//   text2 = "Superman",
+//   text3 = "shaktiman";
+// function constructURL(textt) {
+//   return `https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=${textt}`;
+// }
+
+// function doFetch(url) {
+//   fetch(constructURL(url))
+//     .then((res) => res.json())
+//     .then((json) => console.log(json.contents.text));
+// }
+// let arr = [doFetch(text1), doFetch(text2), doFetch(text3)];
+// arr.forEach((e) => console.log(e));
