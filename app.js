258259fe9aa32a3f2fb5c47d9e54c47e5b4c48e2
Prakash Agrahari <sshivamprakashh301@gmail.com>
2021-04-09 16:12:17 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var buttonTranslate = document.querySelector('#btn')
 var text = document.querySelector('#txt')
+var output = document.querySelector('#output')
+
 function clickEventHandler(){
-    console.log('clicked')
-    console.log('input', text.value)
+    output.innerText = `now this can be outputted here too => ${text.value}`
 }
 
 buttonTranslate.addEventListener("click", clickEventHandler) 
